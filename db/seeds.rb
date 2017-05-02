70db753e21bffd36a79492f9dc4252e621fc7e51
Yerlan <bayekeshov@gmail.com>
2017-05-01 20:25:55 -0400

    Added password

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(:id => 3, :admin => true, :email => 'kgalex0819@gmail.com', :password => '123456')
+User.create(:id => 3, :admin => true, :email => 'kgalex0819@gmail.com', :password => 'BigSpanish3000!')
