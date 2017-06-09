a10b7c7ed0beefc2b375700f4b54dcea28c63510
Yerlan Baiyekeshov <erka@IMac.fios-router.home>
2017-06-09 16:03:15 -0400

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,9 @@ Rails.application.routes.draw do
   resources :presses
   resources :news
   resources :events
-<<<<<<< HEAD
+
   
   get '/news/last/:id'   => 'news#last'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root	'welcome#index'
->>>>>>> 48079c87774fdd677fbd541caf4fb8aa59880d46
 end
